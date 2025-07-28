4e868983573782b823bce582f2c36b51b35adaee
mdsahinsiraj <mdsahinsiraj2@gmail.com>
2025-07-28 17:11:45 +0600

    add agenda logging

--- src/app/modules/Agenda/index.ts
+++ src/app/modules/Agenda/index.ts
@@ -1,11 +1,10 @@
 import agenda from './jobs'
+agenda.on('start', job => console.log(`Job ${job.attrs.name} starting`));
+agenda.on('complete', job => console.log(`Job ${job.attrs.name} finished`));
+agenda.on('fail', (err, job) => console.error(`Job ${job.attrs.name} failed: ${err.message}`));
 
 agenda.on('ready', async () => {
     console.log("Agenda is ready"); 
     agenda.start();
     await agenda.every('5 0 * * *', 'users:prayer');
-});
-
-agenda.on("error", (e) => {
-    console.log(e);
 });
